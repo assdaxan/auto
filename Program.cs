4db43629b068b412f53abe0595f0448c5809cee0
ubun <elaalsdn1@naver.com>
2018-08-24 21:05:53 +0900

    add CreateToolhelp32Snapshot

--- Program.cs
+++ Program.cs
@@ -19,7 +19,11 @@ namespace auto{
         [DllImport("user32.dll")]
         static extern IntPtr GetWindow(IntPtr hWnd, int wCmd);
         [DllImport("user32.dll")]
-        static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString,int nMaxCount);
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+        [DllImport("psapi.dll")]
+        static extern int GetModuleBaseName(IntPtr hProcess, IntPtr hModule, StringBuilder lpBaseName, int nSize);
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateToolhelp32Snapshot(int dwFlags, int th32ProcessID);
 
         // Win API에서 사용되는 변수 정의
         // mouse_event
@@ -43,27 +47,43 @@ namespace auto{
         const int GW_HWNDNEXT = 0x2;
         const int GW_HWNDPREV = 0x3;
         const int GW_OWNER = 0x4;
-
+        //CreateToolhelp32Snapshot
+        const uint TH32CS_INHERIT = 0x80000000;
+        const int TH32CS_SNAPALL = TH32CS_SNAPHEAPLIST | TH32CS_SNAPMODULE | TH32CS_SNAPPROCESS | TH32CS_SNAPTHREAD;
+        const int TH32CS_SNAPHEAPLIST = 0x1;
+        const int TH32CS_SNAPMODULE = 0x8;
+        const int TH32CS_SNAPMODULE32 = 0x10;
+        const int TH32CS_SNAPPROCESS = 0x2;
+        const int TH32CS_SNAPTHREAD = 0x4;
+        const int INVALID_HANDLE_VALUE = -1;
+        const int ERROR_BAD_LENGTH = 0x18;
+        
         static void Main(string[] args){
             Console.WriteLine("UBUN Image Auto");
-            using (Bitmap target = loadImg(@"C:\Users\Administrator\Desktop\target.PNG"))
-                searchImg(target);
+            using (Bitmap target = loadImg(@"C:\Users\Administrator\Desktop\target.PNG")){
+                OpenCvSharp.Point p = searchImg(target);
+                if(p.X != -1 && p.Y != -1){
+                    int width = target.Size.Width;
+                    int height = target.Size.Height;
+                    click(p.X+(width/2), p.Y+(height/2));
+                }
+            }
         }
-        public static IntPtr findHwnd(string targetName){
+        static IntPtr getHwndByCaption(string targetName){
             StringBuilder s = new StringBuilder(256);
             IntPtr hwnd = FindWindow(null, null);
             while(hwnd != IntPtr.Zero){
-                GetWindowText(hwnd, s, 256);
+                GetWindowText(hwnd, s, s.Capacity);
                 if(s.ToString().Equals(targetName))
                     break;
                 hwnd = GetWindow(hwnd, GW_HWNDNEXT);
             }
             return hwnd;
         }
-        public static Bitmap loadImg(string path){
+        static Bitmap loadImg(string path){
             return new Bitmap(path);
         }
-        public static Bitmap ScreenCopy(){
+        static Bitmap ScreenCopy(){
             // 주화면의 크기 정보 읽기
             int width = (int)SystemParameters.PrimaryScreenWidth;
             int height = (int)SystemParameters.PrimaryScreenHeight;
@@ -77,7 +97,7 @@ namespace auto{
             }
             return bmp;
         }
-        public static void searchImg(Bitmap find_img){
+        static OpenCvSharp.Point searchImg(Bitmap find_img){
             using (Bitmap screen_img = ScreenCopy())
             using (Mat ScreenMat = BitmapConverter.ToMat(screen_img))
             using (Mat FindMat = BitmapConverter.ToMat(find_img))
@@ -87,18 +107,17 @@ namespace auto{
                 OpenCvSharp.Point minloc, maxloc;
                 //찾은 이미지의 유사도 및 위치 값을 받습니다. 
                 Cv2.MinMaxLoc(res, out minval, out maxval, out minloc, out maxloc);
+                Console.WriteLine(maxloc);
                 Console.WriteLine("찾은 이미지의 유사도 : " + maxval);
                 //유사도를 0.7 초과로 설정합니다.
                 if (maxval > 0.7){
-                    int width = find_img.Size.Width;
-                    int height = find_img.Size.Height;
-                    //이미지의 중앙을 누름니다.
-                    click(maxloc.X+(width/2), maxloc.Y+(height/2));
                     Console.WriteLine($"{maxloc.X}, {maxloc.Y}");
+                    return maxloc;
                 }
             }
+            return new OpenCvSharp.Point(-1, -1);
         }
-        public static void click(int x, int y){
+        static void click(int x, int y){
             SetCursorPos(x, y);
             mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
             mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
